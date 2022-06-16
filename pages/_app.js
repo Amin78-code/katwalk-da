7cc710dafd5b788cd926677cd0c647c0fff36f6e
Amin_Cs <aminnoor.cs@gmail.com>
2022-06-16 19:03:02 +0500

    15g

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
-      </Head>
+      </Head>hjk
       <Component {...pageProps} />
     </>
   );
